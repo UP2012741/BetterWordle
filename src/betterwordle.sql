ea8196728affbab807a167fc5659773a295f14a3
UP2012741 <YuyutsuRai@outlook.com>
2022-05-07 13:52:04 +0100

    Redid the layout of the files and also adding an insertion to the betterwordle.sql

--- src/betterwordle.sql
+++ src/betterwordle.sql
@@ -1,11 +1,11 @@
 CREATE EXTENSION IF NOTS "uuid-ossp";
 
-DROP TABLE IF EXISTS betterwordle;
+DROP TABLE IF EXISTS words;
 
 CREATE TABLE IF NOT EXISTS words(
     id  uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    word VARCHAR(5);
+    word CHAR(5);
 );
 
-
-
+INSERT INTO words(word) VALUES
+()

20138587349fda6fc7ce8193d18c5dbc5c87bf63
UP2012741 <YuyutsuRai@outlook.com>
2022-05-15 00:05:02 +0100

    CHANGED THE WHOLE BACK END TO USE SQL

--- src/config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  // remove the next line if not running on our VM and connection to PostgreSQL doesn't work
-  database: 'betterwordle',
-  statement_timeout: 5000,
-};
---
+++ src/migrations-sqlite/001-inital.sql
@@ -0,0 +1,13 @@
+-- UP
+CREATE TABLE Words{
+    id CHAR(36) PRIMARY KEY,
+    word TEXT   NOT NULL
+}
+
+
+
+
+
+-- Down
+
+DROP TABLE Words
